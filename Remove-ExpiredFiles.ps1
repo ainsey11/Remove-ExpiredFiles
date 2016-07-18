d3e12051060f0c444b39fdb585ed865f0b3fed0b
Ainsey11 <robert@ainsey11.com>
2016-07-18 10:01:01 +0100

    Initial Commit

---
+++ Remove-ExpiredFiles.ps1
@@ -0,0 +1,17 @@
+﻿<#
+.Synopsis
+   Removes Files older than 30 days from a set directory
+.EXAMPLE
+    This script has no params for running at cli. All params are set within the ps1 file.
+.NOTES
+    Author - Robert Ainsworth - https://ainsey11.com
+#>
+
+$Filepath = "C:\Test"
+$MaxAge = (Get-Date).AddDays(-30)
+
+# Delete files older than the $limit.
+Get-ChildItem -Path $Filepath -Recurse -Force | Where-Object { !$_.PSIsContainer -and $_.CreationTime -lt $maxage } | Remove-Item -Force
+
+# Delete any empty directories left behind after deleting the old files.
+Get-ChildItem -Path $Filepath -Recurse -Force | Where-Object { $_.PSIsContainer -and (Get-ChildItem -Path $_.FullName -Recurse -Force | Where-Object { !$_.PSIsContainer }) -eq $null } | Remove-Item -Force -Recurse
